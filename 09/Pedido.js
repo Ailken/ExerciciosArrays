570ac060fdedadd1800c4c88321b306df5a72b15
Ailken <ailkencf@gmail.com>
2023-07-30 13:05:00 -0300

    exercicios de Arrays(exercicio 9)

---
+++ 09/Pedido.js
@@ -0,0 +1,6 @@
+const filaDePedidos = ["pedido 12", "pedido 13", "pedido 14"];
+
+const pedidoAnterior = filaDePedidos.shift();
+console.log(filaDePedidos);
+const novoPedido = filaDePedidos.push("pedido 15");
+console.log(filaDePedidos);
