33bfd0035d90d5c138435bacc7ebe807d88e9153
Ailken <ailkencf@gmail.com>
2023-07-30 09:38:30 -0300

    exercicios de Arrays(exercicio 5)

--- 05/index.js
+++ 05/index.js
@@ -1,1 +1,12 @@
-const original = [1, 4, 12, 21, 53, 88, 112];
+const original = [1, 4, 12, 21, 53, 88, 112, 26];
+
+const par = x => x % 2 === 0;
+
+let pares = [];
+
+for (i = 0; i < original.length; i++) {
+    if (par(original[i])) {
+        pares.push(original[i]);
+    }
+}
+console.log(pares);
