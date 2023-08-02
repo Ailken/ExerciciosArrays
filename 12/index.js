98533ee8917c66f11b93721be606502111507609
Ailken <ailkencf@gmail.com>
2023-08-02 14:29:57 -0300

    exercicios de Arrays(exercicio 12)

--- 12/index.js
+++ 12/index.js
@@ -1,3 +1,16 @@
-const filaDeDentro = ["Jose", "Maria", "Joao"];
-const filaDeFora = ["Joana", "Roberta", "Marcos", "Felipe"];
+let filaDeDentro = ["Jose", "Maria", "Joao"];
+let filaDeFora = ["Joana", "Roberta", "Marcos", "Felipe"];
+//1. Verificar quantas pessoas podem ser transferidas para a filaDeDentro
+//2. transferencia de pessoas da filaDeFora para a filaDeDentro até que o limite seja alcançado;
+//3. imprimir a fila de dentro e a de fora atualizadas
 
+const limiteFilaDentro = 6;
+
+const espacosDisponiveis = limiteFilaDentro - filaDeFora.length;
+
+filaDeDentro = filaDeDentro.concat(filaDeFora.slice(0, espacosDisponiveis));
+filaDeFora = filaDeFora.slice(espacosDisponiveis);
+
+console.log(filaDeDentro);
+
+console.log(filaDeFora);
